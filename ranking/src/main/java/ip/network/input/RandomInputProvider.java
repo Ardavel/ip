4f04a11cabf878b021a599621d7a0724c774a386
Lukasz Cyran <180519@edu.p.lodz.pl>
2015-04-27 21:54:55 +0200

    Poprawka generatora, 1 przedzial losowy, reszta malejacy udzial procentowy

--- ranking/src/main/java/ip/network/input/RandomInputProvider.java
+++ ranking/src/main/java/ip/network/input/RandomInputProvider.java
@@ -6,6 +6,8 @@
 package ip.network.input;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -18,6 +20,8 @@ public class RandomInputProvider implements InputProvider {
     protected List<InputRow> dataset;
 
     int nextRow = 0;
+    
+    private int inputSize = 14;
 
     Random random = new Random();
 
@@ -30,27 +34,36 @@ public class RandomInputProvider implements InputProvider {
 
     private List<InputRow> generateRandomInput(int dataSize) {
         List<InputRow> randomDataset = new ArrayList<>();
-        double[] randomInputs = new double[14];
+        Double[] randomInputs = new Double[inputSize];
         double[] randomOutputs = new double[1];
         for (int i = 0; i < dataSize; i++) {
             //Gwałtowne hamowania i przyspieszenia na minutę (0-10)
-            for (int j = 0; j < 2; j++) {
-                randomInputs[j] = random.nextInt(11);
+            for(int j=0; j<2; j++){
+                randomInputs[j] = 1d * random.nextInt(11);
                 System.out.println(j + " " + randomInputs[j]);
             }
-            double[] tempRandomOutputs = new double[12];
+            Double[] tempRandomInputs = new Double[inputSize - 2];
             double tempSum = 0;
             //Procentowy czas przebywania w każdym z przedziałów rpm
-            for (int j = 2; j < 14; j++) {
-                tempRandomOutputs[j - 2] = random.nextDouble();
-                tempSum += tempRandomOutputs[j - 2];
+            for(int j=2; j<inputSize; j++){
+                tempRandomInputs[j-2] = random.nextDouble();
+                tempSum += tempRandomInputs[j-2];
             }
-            for (int j = 2; j < 14; j++) {
-                randomInputs[j] = tempRandomOutputs[j - 2] / tempSum;
-                System.out.println(j + " " + randomInputs[j]);
+            for(int j=2; j<inputSize; j++){
+                randomInputs[j] = tempRandomInputs[j-2]/tempSum;
             }
+            
+            Arrays.sort(randomInputs, 3, inputSize, Collections.reverseOrder());
+            
+            double[] finalRandomInputs = new double[inputSize];
+            
+            for(int j=0; j<inputSize; j++){
+                finalRandomInputs[j] = randomInputs[j].doubleValue();
+                System.out.println(j + " " + finalRandomInputs[j]);
+            }
+            
             randomOutputs[0] = Double.NaN;
-            randomDataset.add(new InputRow(randomInputs, randomOutputs));
+            randomDataset.add(new InputRow(finalRandomInputs, randomOutputs));
         }
         return randomDataset;
     }
