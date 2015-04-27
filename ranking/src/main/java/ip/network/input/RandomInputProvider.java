75e288ccfa81512f88488c91f5dc0d405df1194f
Lukasz Cyran <180519@edu.p.lodz.pl>
2015-04-27 22:11:58 +0200

    Normalizacja wektora inputow

--- ranking/src/main/java/ip/network/input/RandomInputProvider.java
+++ ranking/src/main/java/ip/network/input/RandomInputProvider.java
@@ -20,7 +20,7 @@ public class RandomInputProvider implements InputProvider {
     protected List<InputRow> dataset;
 
     int nextRow = 0;
-    
+
     private int inputSize = 14;
 
     Random random = new Random();
@@ -30,6 +30,7 @@ public class RandomInputProvider implements InputProvider {
 
     public RandomInputProvider(int dataSize) {
         dataset = new ArrayList<>(generateRandomInput(dataSize));
+        normalize();
     }
 
     private List<InputRow> generateRandomInput(int dataSize) {
@@ -38,34 +39,48 @@ public class RandomInputProvider implements InputProvider {
         double[] randomOutputs = new double[1];
         for (int i = 0; i < dataSize; i++) {
             //Gwałtowne hamowania i przyspieszenia na minutę (0-10)
-            for(int j=0; j<2; j++){
+            for (int j = 0; j < 2; j++) {
                 randomInputs[j] = 1d * random.nextInt(11);
-                System.out.println(j + " " + randomInputs[j]);
             }
             Double[] tempRandomInputs = new Double[inputSize - 2];
             double tempSum = 0;
             //Procentowy czas przebywania w każdym z przedziałów rpm
-            for(int j=2; j<inputSize; j++){
-                tempRandomInputs[j-2] = random.nextDouble();
-                tempSum += tempRandomInputs[j-2];
+            for (int j = 2; j < inputSize; j++) {
+                tempRandomInputs[j - 2] = random.nextDouble();
+                tempSum += tempRandomInputs[j - 2];
             }
-            for(int j=2; j<inputSize; j++){
-                randomInputs[j] = tempRandomInputs[j-2]/tempSum;
+            for (int j = 2; j < inputSize; j++) {
+                randomInputs[j] = tempRandomInputs[j - 2] / tempSum;
             }
-            
+
             Arrays.sort(randomInputs, 3, inputSize, Collections.reverseOrder());
-            
+
             double[] finalRandomInputs = new double[inputSize];
-            
-            for(int j=0; j<inputSize; j++){
-                finalRandomInputs[j] = randomInputs[j].doubleValue();
-                System.out.println(j + " " + finalRandomInputs[j]);
+
+            for (int j = 0; j < inputSize; j++) {
+                finalRandomInputs[j] = randomInputs[j];
+//                System.out.println(j + " = " + finalRandomInputs[j]);
             }
-            
+
             randomOutputs[0] = Double.NaN;
             randomDataset.add(new InputRow(finalRandomInputs, randomOutputs));
         }
         return randomDataset;
+    }
+
+    private void normalize() {
+        for (InputRow dataset1 : dataset) {
+            double length = 0;
+            double[] inputs = dataset1.getValues();
+            for(int j=0; j<inputSize; j++){
+                length += inputs[j] * inputs[j];
+            }
+            length = Math.sqrt(length);
+            for(int j=0; j<inputSize; j++){
+                inputs[j] = inputs[j] / length;
+//                System.out.println(j + " " + inputs[j]);
+            }
+        }
     }
 
     @Override
